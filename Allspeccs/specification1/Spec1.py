6a7ebdb5ca60716a5d4b68c9aa5912e970d2f831
glen arch <g.p.arch1@newcastle.ac.uk>
2019-11-05 15:00:43 +0000

    updated the format of the csv file

--- Allspeccs/specification1/Spec1.py
+++ Allspeccs/specification1/Spec1.py
@@ -2,6 +2,7 @@ streamreader = open("text.txt", "r")
 characters = {}
 dictionary = {}
 word = ""
+file = ""
 for line in streamreader:
     for character in line:
         character.lower()
@@ -18,11 +19,17 @@ for line in streamreader:
                 if word in dictionary:
                     current_count = dictionary[word]
                     dictionary[word] = str(int(current_count) + 1)
+                    for i in range(0, len(file)):
+                        if file[i:i+len(word)] == word:
+                            filelist = list(file)
+                            filelist[i+len(word)+2] = current_count
+                            file = ''.join(filelist)
                 else:
                     dictionary[word] = "1"
+                    file += word + ", 1 \n"
                 word = ""
         else:
             word += character
 streamreader.close()
 streamwriter = open("csv.txt", "w")
-streamwriter.write(str(dictionary))
+streamwriter.write(file)
