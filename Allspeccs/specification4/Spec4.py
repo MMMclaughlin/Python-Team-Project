4e52c8c7baf88c3fb901aec7629c4c8f968adcb9
b9037989 <b9037989@newcastle.ac.uk>
2019-11-12 04:11:29 +0000

    Added window, layout, background, buttons. Working with PYQT5.

--- Allspeccs/specification4/Spec4.py
+++ Allspeccs/specification4/Spec4.py
@@ -1,129 +1,69 @@
-def test():
-    print("this is working spec 4")
-
-# TEST TEST TEST
-
-# import sys
-# from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
-# from PyQt5.QtGui import QIcon
-# from PyQt5.QtCore import pyqtSlot
-#
-#
-# class App(QMainWindow):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.title = 'Practical 3, Newcastle University'
-#         self.left = 1080
-#         self.top = 650
-#         self.width = 800
-#         self.height = 400
-#         self.initUI()
-#
-#     def initUI(self):
-#         self.setWindowTitle(self.title)
-#         self.setGeometry(self.left, self.top, self.width, self.height)
-#
-#         mainMenu = self.menuBar()
-#         fileMenu = mainMenu.addMenu('File')
-#         editMenu = mainMenu.addMenu('Edit')
-#         viewMenu = mainMenu.addMenu('View')
-#         searchMenu = mainMenu.addMenu('Search')
-#         toolsMenu = mainMenu.addMenu('Tools')
-#         helpMenu = mainMenu.addMenu('Help')
-#
-#         exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-#         exitButton.setShortcut('Ctrl+Q')
-#         exitButton.setStatusTip('Exit application')
-#         exitButton.triggered.connect(self.close)
-#         fileMenu.addAction(exitButton)
-#
-#         undoButton = QAction(QIcon('exit24.png'), 'Undo', self)
-#         undoButton.setShortcut('Ctrl+M')
-#         undoButton.setStatusTip('Exit Z application')
-#         undoButton.triggered.connect(self.close)
-#         editMenu.addAction(undoButton)
-#
-#         graduationButton = QAction(QIcon('exit24.png'), 'GRADUATION', self)
-#         graduationButton.setShortcut('Ctrl+G')
-#         graduationButton.setStatusTip('GRADUATE!')
-#         graduationButton.triggered.connect(self.close)
-#         helpMenu.addAction(graduationButton)
-#
-#         # Create textbox
-#         self.textbox = QLineEdit(self)
-#         self.textbox.move(20, 20)
-#         self.textbox.resize(280, 40)
-#
-#         # Create a button in the window
-#         self.button = QPushButton('Show text', self)
-#         self.button.move(20, 80)
-#
-#         # connect button to function on_click
-#         self.button.clicked.connect(self.on_click)
-#         self.show()
-#
-#
-#
-#     @pyqtSlot()
-#     def on_click(self):
-#         textboxValue = self.textbox.text()
-#         QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok,
-#                              QMessageBox.Ok)
-#         self.textbox.setText("")
-#
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ex = App()
-#     sys.exit(app.exec_())
-
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
-from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QDialog
+from PyQt5.QtGui import QPainter, QColor, QPen, QIcon, QPixmap
+from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
 import random
+import webbrowser
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt paint - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 440
-        self.height = 280
+        self.title = 'Newcastle University Services Shortcuts'
+        self.left = 500
+        self.top = 500
+        self.width = 1000
+        self.height = 500
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # Set window background color
         self.setAutoFillBackground(True)
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.white)
         self.setPalette(p)
 
-        # Add paint widget and paint
-        self.m = PaintWidget(self)
-        self.m.move(0,0)
-        self.m.resize(self.width,self.height)
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('hero-locations.jpg'))
+        self.label.setGeometry(0,0,1000,500)
+
+        def open_webbrowser():
+            webbrowser.open('http://www.google.com')
+
+        button1 = QPushButton('BLACKBOARD', self)
+        button1.setToolTip('Lecture ReCaps and learning materials')
+        button1.setGeometry(120, 80, 120, 60)
+        button1.clicked.connect(webbrowser.open)
+
+        button2 = QPushButton('NUCODE', self)
+        button2.setToolTip('GitHub for students (GitLab)')
+        button2.setGeometry(120, 160, 120, 60)
+        #button2.clicked.connect(self.on_click2)
+
+        button3 = QPushButton('NESS', self)
+        button3.setToolTip('Place for submitting your coursework')
+        button3.setGeometry(120, 240, 120, 60)
+        #button3.clicked.connect(self.on_click3)
+
+        button4 = QPushButton('RAS', self)
+        button4.setToolTip('Remote Application Service')
+        button4.setGeometry(120, 320, 120, 60)
+        #button4.clicked.connect(self.on_click4)
+
+        button5 = QPushButton('PORTFOLIO', self)
+        button5.setToolTip('Your student portfolio')
+        button5.setGeometry(120, 400, 120, 60)
+        #button5.clicked.connect(self.on_click5)
 
         self.show()
 
-class PaintWidget(QWidget):
-    def paintEvent(self, event):
-        qp = QPainter(self)
-
-        qp.setPen(Qt.black)
-        size = self.size()
-
-        for i in range(1024):
-            x = random.randint(1, size.width()-2)
-            y = random.randint(1, size.height()-2)
-            qp.drawPoint(x, y)
+    @pyqtSlot()
+    def on_click1(self):
+        print('ok')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
